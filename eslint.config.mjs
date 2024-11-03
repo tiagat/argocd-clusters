616dbcb8651616699dbed3b79565cb163869c7ae
Ruslan Sakal <ruslan.sakal@gmail.com>
2024-11-03 10:55:29 +0100

    Added eslint.config.mjs file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,21 @@
+import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 
 export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    rules: {
-      'no-console': 'error',
+    languageOptions: {
+      globals: globals.builtin,
+
     },
+    plugins: {
+      unicorn: eslintPluginUnicorn,
+    },
+    rules: {
+      'no-console': 'error'
+    }
   },
 ];
